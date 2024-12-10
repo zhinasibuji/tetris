fb6d43f9d83be030d5ebd180335f892e25c29266
Administrator <xyj784516912@gmail.com>
2024-12-10 09:32:16 +0800

    Changes

--- scene_map.py
+++ scene_map.py
@@ -36,10 +36,6 @@ class SceneMap:
     def __init__(self) -> None:
         self.squares = []
 
-    #仅测试用代码
-    def print_squares(self):
-        print([(square.x, square.y) for square in self.squares])
-
     def call(self) -> None:
         self.create_squareset()
         while True:
---
+++ test.py
@@ -0,0 +1,3 @@
+# 仅测试用代码
+def print_squares(self):
+    print([(square.x, square.y) for square in self.squares])
