476a17a3bb4334cfac129401b08fecd044201a91
Administrator <xyj784516912@gmail.com>
2025-01-19 13:01:34 +0800

    1.4 脱离了对numpy的依赖

--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
 from scene_title import SceneTitle
 
-if __name__ == '__main__':
-    scene = SceneTitle()
+scene = SceneTitle()
+scene.call()
+while True:
+    scene = scene.next_scene
     scene.call()
-    while True:
-        scene = scene.next_scene
-        scene.call()
